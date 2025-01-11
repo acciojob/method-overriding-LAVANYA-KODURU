afefd96b6fb7bacc6ff527966fb8b034edee1d0e
Lavanya Lakshmi Koduru <141067145+LAVANYA-KODURU@users.noreply.github.com>
2025-01-11 22:11:12 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
-
+public class A {
+  public String meth(){
+  return "Invoking method from Class A";
+  }
+}
+public class B extends A {
+  public String meth(){
+    return "Method is ovverridden in Extended class B";
+  }
+}
 public class Main {
-  
+  public static void main(String[] args){
+    B obj=new B();
+    System.out.println(obj.meth());
+  }
 }
